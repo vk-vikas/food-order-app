9b3cc2cac7eb69c64ddf2224d6b7cbae9dd08f51
vikas kumar <vk.vikas3003@gmail.com>
2023-02-12 22:37:40 +0530

    loading state added before data is fetched from backend

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,34 +1,45 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-
 const AvailableMeals = () => {
   //we will get data after the component is rendered so we need to use state
   // for our fetched data so that when we recieve the data , we rerender it
-  const [meals,setMeals] = useState([]);
-  useEffect(()=> {
-    const fetchData = async ()=> {
-      const response = await fetch('https://udemy-react-edf79-default-rtdb.firebaseio.com//meals.json')
+  const [meals, setMeals] = useState([]);
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://udemy-react-edf79-default-rtdb.firebaseio.com//meals.json"
+      );
       const responseData = await response.json();
       // returned data is object , we need array
       const loadedMeals = [];
 
-      for(const key in responseData){
+      for (const key in responseData) {
         loadedMeals.push({
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
-          price: responseData[key].price
-        })
+          price: responseData[key].price,
+        });
       }
 
       setMeals(loadedMeals);
-    }
+      setIsLoading(false);
+    };
     fetchData();
-  },[])
+  }, []);
 
+  if(isloading){
+    return (
+      <div className={classes.mealsLoading}>
+        <h3>Loading...</h3>
+      </div>
+    )
+  }
   const mealsList = meals.map((i) => (
     <MealItem
       key={i.id}
@@ -38,16 +49,12 @@ const AvailableMeals = () => {
       price={i.price}
     />
   ));
- 
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}
-
-        </ul>
-        
+        <ul>{mealsList}</ul>
       </Card>
-      
     </section>
   );
 };
